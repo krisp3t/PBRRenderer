037012884f85fbe17e1372aa887a108f901f7f7f
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-06-07 20:10:57 +0200

    Add message pump

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,18 +1,33 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(
+    HWND hWnd,
+    UINT msg,
+    WPARAM wParam,
+    LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_CLOSE:
+        PostQuitMessage(42);
+        DestroyWindow(hWnd);
+        break;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-
     const auto pClassName = "PBRRenderer";
     // Register window class
     WNDCLASSEX wc = {0};
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -24,5 +39,31 @@ int CALLBACK WinMain(
     wc.hIconSm = nullptr;
     RegisterClassEx(&wc);
 
-    return 0;
+    // Create window instance
+    HWND hWnd = CreateWindowEx(
+        0,
+        pClassName,
+        pClassName,
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        200, 200, 800, 600,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+        );
+    ShowWindow(hWnd, SW_SHOW);
+
+    // Message pump
+    MSG msg;
+    BOOL gResult;
+    while ((gResult = GetMessage(&msg, nullptr, 0, 0) > 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    if (gResult == -1)
+    {
+        return -1;
+    }
+    return msg.wParam;
 }
