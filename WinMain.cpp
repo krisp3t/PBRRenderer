fb84f5b9b63c529f07551ac2fbd9728ff301af6b
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-06-07 19:09:42 +0200

    Modify WinMain

--- WinMain.cpp
+++ WinMain.cpp
@@ -6,6 +6,23 @@ int CALLBACK WinMain(
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    while (true);
+
+    const auto pClassName = "PBRRenderer";
+    // Register window class
+    WNDCLASSEX wc = {0};
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = DefWindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = pClassName;
+    wc.hIconSm = nullptr;
+    RegisterClassEx(&wc);
+
     return 0;
 }
